d509f5ce719a3ce1de75241f1503f9a2a6324c95
Simon Chemouil <simon.chemouil@lambdacube.fr>
2017-08-30 15:38:32 +0200

    Issue #4: Make Exception type in FallibleFunction parametric

--- src/main/java/io/primeval/common/function/FallibleFunction.java
+++ src/main/java/io/primeval/common/function/FallibleFunction.java
@@ -1,7 +1,7 @@
 package io.primeval.common.function;
 
 @FunctionalInterface
-public interface FallibleFunction<T, R> {
+public interface FallibleFunction<T, R, E extends Throwable> {
 
-    R apply(T t) throws Exception;
+    R apply(T t) throws E;
 }
